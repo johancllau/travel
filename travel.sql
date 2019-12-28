0a2cafe7cc6df3622bc68a8272b75ce8c437f2fa
johancllau <cllaujhohan@gmail.com>
2019-12-28 13:59:36 +0700

    uppdate db

--- travel.sql
+++ travel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2019 at 07:04 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Generation Time: Dec 28, 2019 at 07:58 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,35 +25,163 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `kode_booking` int(11) NOT NULL,
+  `tanggal_tour` varchar(20) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `id_paket` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `destinasi`
+--
+
+CREATE TABLE `destinasi` (
+  `id_destinasi` int(11) NOT NULL,
+  `nama_destinasi` varchar(100) NOT NULL,
+  `alamat_destinasi` varchar(100) NOT NULL,
+  `image_destinasi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mobil_travell`
+--
+
+CREATE TABLE `mobil_travell` (
+  `kode_travell` int(11) NOT NULL,
+  `kapasitas` int(11) NOT NULL,
+  `image_travell` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paket_tour`
+--
+
+CREATE TABLE `paket_tour` (
+  `id_paket` int(11) NOT NULL,
+  `nama_paket` varchar(100) NOT NULL,
+  `price_paket` int(11) NOT NULL,
+  `id_destinasi` int(11) NOT NULL,
+  `kode_travell` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `nama` varchar(255) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `jk` enum('pria','wanita') NOT NULL,
-  `notelp` int(13) NOT NULL,
-  `status` enum('1','0') NOT NULL DEFAULT '0'
+  `gender` enum('pria','wanita') NOT NULL,
+  `notelp` int(12) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`username`, `password`, `nama`, `email`, `jk`, `notelp`, `status`) VALUES
-('fariza', 'walid', 'Muh Fariza', 'muhfariza98@gmail.com', '', 2147483647, '');
+INSERT INTO `user` (`username`, `password`, `name`, `email`, `gender`, `notelp`, `address`, `status`) VALUES
+('Admin', 'admin123', 'johan cllau', 'cllaujhohan@gmail.com', 'pria', 2147483647, 'janti', '1'),
+('Galang krsnt', 'galang123', 'Galang Krisnanto', 'Galangkrsnt@gmail.com', 'pria', 2147483647, 'bantul', '0');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`kode_booking`),
+  ADD KEY `username` (`username`),
+  ADD KEY `id_paket` (`id_paket`);
+
+--
+-- Indexes for table `destinasi`
+--
+ALTER TABLE `destinasi`
+  ADD PRIMARY KEY (`id_destinasi`);
+
+--
+-- Indexes for table `mobil_travell`
+--
+ALTER TABLE `mobil_travell`
+  ADD PRIMARY KEY (`kode_travell`);
+
+--
+-- Indexes for table `paket_tour`
+--
+ALTER TABLE `paket_tour`
+  ADD PRIMARY KEY (`id_paket`),
+  ADD KEY `id_destinasi` (`id_destinasi`),
+  ADD KEY `kode_travell` (`kode_travell`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `kode_booking` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `destinasi`
+--
+ALTER TABLE `destinasi`
+  MODIFY `id_destinasi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mobil_travell`
+--
+ALTER TABLE `mobil_travell`
+  MODIFY `kode_travell` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `paket_tour`
+--
+ALTER TABLE `paket_tour`
+  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
+  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`id_paket`) REFERENCES `paket_tour` (`id_paket`);
+
+--
+-- Constraints for table `paket_tour`
+--
+ALTER TABLE `paket_tour`
+  ADD CONSTRAINT `paket_tour_ibfk_1` FOREIGN KEY (`id_destinasi`) REFERENCES `destinasi` (`id_destinasi`),
+  ADD CONSTRAINT `paket_tour_ibfk_2` FOREIGN KEY (`kode_travell`) REFERENCES `mobil_travell` (`kode_travell`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
